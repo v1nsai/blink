ae947837039c3d5e836fbead042009ae695e0115
doctor_ew <doctor_ew@protonmail.com>
2021-05-04 17:12:37 -0400

    Troubleshooting delays not working

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@
 
 use ruduino::Pin;
 use ruduino::cores::current::{port};
+// use ruduino::cores::atmega328p::{port};
+use avr_delay::delay_ms;
 
 #[no_mangle]
 pub extern fn main() {
     port::B5::set_output();
-
+    
     loop {
         port::B5::set_high();
-
-        ruduino::delay::delay_ms(1000);
+        
+        delay_ms(1000);
 
         port::B5::set_low();
 
-        ruduino::delay::delay_ms(1000);
+        delay_ms(1000);
     }
 }
